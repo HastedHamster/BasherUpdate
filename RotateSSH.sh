f2d2066c704653fc0d8a077854067cfafcbcc08f
Hasted <63086606+HastedHamster@users.noreply.github.com>
2021-05-11 13:32:24 -0700

    Update RotateSSH.sh

--- RotateSSH.sh
+++ RotateSSH.sh
@@ -1,6 +1,11 @@
-sudo mkdir /etc/ssh/default_keys
-sudo mv /etc/ssh/ssh_host_* /etc/ssh/default_keys/
+sudo mkdir /etc/ssh/old_keys
+sudo mv /etc/ssh/ssh_host_* /etc/ssh/old_keys/
+echo "Generating new keys..."
 sudo dpkg-reconfigure openssh-server
-sudo md5sum /etc/ssh/ssh_host_* && sudo md5sum /etc/ssh/default_keys/*
+echo "New Keys:"
+sudo md5sum /etc/ssh/ssh_host_* 
+echo "Old Keys:"
+sudo md5sum /etc/ssh/old_keys/*
+echo "Restarting SSH service..."
 sudo systemctl restart ssh && sudo systemctl status ssh
 echo "Exiting script..."
