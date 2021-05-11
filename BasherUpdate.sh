25d201cfe34e3aa881deb3c0875c94a9b7c3fd61
Hasted <jesse.olson@avanade.com>
2021-05-11 12:13:11 -0700

    Updated echo lines

--- BasherUpdate.sh
+++ BasherUpdate.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/bash
+
+echo "RUNNING: sudo apt update --fix-missing"
 sudo apt clean
 sudo apt update --fix-missing
-echo "RUNNING: sudo apt update --fix-missing"
 echo "COMPLETED: Updates"
 
+echo "RUNNING: sudo apt dist-upgrade -y"
 sudo apt dist-upgrade -y
-echo "sudo apt dist-upgrade -y"
+echo "RUNNING: sudo apt full-upgrade -y"
 sudo apt full-upgrade -y
-echo "sudo apt full-upgrade -y"
 echo "COMPLETED: Upgrades"
 
+echo "RUNNING: sudo apt autoremove -y"
 sudo apt autoremove -y
-echo "sudo apt autoremove -y"
 echo "COMPLETED: Autoremove cleanup"
 echo "Exiting BasherUpdate..."
