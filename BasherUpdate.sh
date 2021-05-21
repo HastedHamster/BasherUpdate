491bff4621d262206a5ea61ef6c57ab8a9c55e0b
Hasted <63086606+HastedHamster@users.noreply.github.com>
2021-05-21 09:02:48 -0700

    Update BasherUpdate.sh

--- BasherUpdate.sh
+++ BasherUpdate.sh
@@ -1,43 +1,45 @@
 #!/usr/bin/bash
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 #Region --- Updates
-echo "RUNNING: sudo apt update --fix-missing"
+echo "${RED}RUNNING: sudo apt update --fix-missing${NC}"
 sudo apt clean
 sudo apt update --fix-missing
 status=$?
 echo $status
 #if status -eq 0
 #then 
-echo "COMPLETED: sudo apt update --fix-missing"
+echo "${RED}COMPLETED: sudo apt update --fix-missing${NC}"
 #else 
 #echo "FAILED: sudo apt update --fix-missing"
 #fi
 
 #Region --- Distro Upgrades
 #echo "RUNNING: sudo apt dist-upgrade -y"
-echo "RUNNING: sudo apt full-upgrade -y"
+echo "${RED}RUNNING: sudo apt full-upgrade -y${NC}"
 sudo apt full-upgrade -y
 status=$?
 echo $status
 #if test status -eq 0
 #then
-echo "COMPLETED: sudo apt full-upgrade -y"
+echo "${RED}COMPLETED: sudo apt full-upgrade -y${NC}"
 #else 
 #echo "FAILED: sudo apt full-upgrade -y"
 #fi
 
 #Region
-echo "RUNNING: sudo apt autoremove -y"
+echo "${RED}RUNNING: sudo apt autoremove -y${NC}"
 sudo apt autoremove -y
 status=$?
 echo $status
 #if test status -eq 0
 #then 
-echo "COMPLETED: sudo apt autoremove -y"
+echo "${RED}COMPLETED: sudo apt autoremove -y${NC}"
 #else
 #echo "FAILED: sudo apt autoremove -y"
 #fi
 
 #Region --- Exiting BasherUpdate
-echo "Exiting BasherUpdate..."
+echo "${RED}Exiting BasherUpdate...${NC}"
 exit 0
