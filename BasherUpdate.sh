63ef03690b72ea4db775f19d30ccd2013ef9c4d2
Hasted <jesse.olson@avanade.com>
2021-05-22 18:50:37 -0700

    RM a line

--- BasherUpdate.sh
+++ BasherUpdate.sh
@@ -24,7 +24,6 @@ fi
 done
 
 #Region --- Distro Upgrades
-	echo -e "RUNNING: sudo apt dist-upgrade -y"
 	echo -e "${PURPLE}RUNNING: sudo apt full-upgrade -y${NC}"
 	sudo apt full-upgrade -y
 	status=$?
