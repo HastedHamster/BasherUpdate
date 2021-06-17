66da0d15df863a378319d8b8350bb316ae6fa97e
Hasted <63086606+HastedHamster@users.noreply.github.com>
2021-06-17 16:31:35 -0700

    Update UFWsetup.sh

--- UFWsetup.sh
+++ UFWsetup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
-
 sudo apt install ufw gufw
 sudo ufw enable
-sudo ufw allow 45643/tcp
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow from 192.168.1.0/24 to any port 45643/tcp
 sudo ufw deny 22/tcp
-sudo ufw status
+sudo ufw status numbered verbose
 sudo ifconfig
-
 exit 0
